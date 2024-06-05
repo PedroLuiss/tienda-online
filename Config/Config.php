3245bd12ef0ec99d85a3b09509d3283503f67279
Pedro Rodriguez <78929255+PedroLuiss@users.noreply.github.com>
2024-06-05 13:38:46 -0400

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,10 +1,10 @@
 <?php
 const BASE_URL = "https://webtiendapedro.netlify.app/"; // URL del proyecto
 
-const HOST = "localhost";
-const USER = "root";
-const PASS = "";
-const DB = "shop";
+const HOST = "sql109.infinityfree.com";
+const USER = "if0_36382551";
+const PASS = "Barquisimeto_29";
+const DB = "if0_36382551_tienda";
 const CHARSET = "charset=utf8";
 const TITLE = "SHOP";
 const MONEDA = "USD";
